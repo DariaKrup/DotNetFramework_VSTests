09f87535948daba6bbe3aafc9094d8393a849c52
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2024-03-14 13:34:50 +0100

    Add solution files

---
+++ dsplname.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestProject2
+{
+    public class NUnitTest1wthDisplN
+    {
+
+
+[TestCase(5,TestName="dsplNameNUNitver1")]
+public void TestCasewithDsplMNameNunitver1() {
+    Assert.AreEqual(5, 6);
+}
+
+        
+        
+        
+    }
+}
